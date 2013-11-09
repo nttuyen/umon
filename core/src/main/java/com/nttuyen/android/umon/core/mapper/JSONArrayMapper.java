bbf7f272061adb0c23efb6011d196316a276eea0
nttuyen <nttuyen_it@yahoo.com>
2013-11-10 01:07:08 +0700

    refactor model mapper

--- core/src/main/java/com/nttuyen/android/umon/core/mapper/JSONArrayMapper.java
+++ core/src/main/java/com/nttuyen/android/umon/core/mapper/JSONArrayMapper.java
@@ -1,6 +1,7 @@
 package com.nttuyen.android.umon.core.mapper;
 
 import android.util.Log;
+import com.nttuyen.android.umon.core.json.JSONUtil;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -26,106 +27,73 @@ public class JSONArrayMapper<Target> implements Mapper<JSONArray, Target> {
 
 	@Override
 	public Target map(JSONArray source, Target target) {
-
-		if(source == null || target == null) {
+		if(source == null || target == null || !target.getClass().isArray()) {
 			return target;
 		}
-
-		if(this.jsonObjectMapper == null) {
-			this.jsonObjectMapper = new JSONObjectMapper();
-			this.jsonObjectMapper.setJsonArrayMapper(this);
-		}
+		prepairJSONObjectMapper();
 
 		Class targetClass = target.getClass();
-		if(!targetClass.isArray()) {
-			return target;
+		Class targetType = targetClass.getComponentType();
+
+		int sourceSize = source.length();
+
+		int targetSize = Array.getLength(target);
+		if(targetSize == 0) {
+			target = (Target)Array.newInstance(targetType, sourceSize);
+			targetSize = Array.getLength(target);
 		}
 
-			int sourceSize = source.length();
-			Class type = targetClass.getComponentType();
-			int targetSize = Array.getLength(target);
-			if(targetSize == 0) {
-				target = (Target)Array.newInstance(type, sourceSize);
-				targetSize = Array.getLength(target);
+		boolean isPrimitive = JSONUtil.isPrimaryType(targetType);
+
+		int size = sourceSize < targetSize ? sourceSize : targetSize;
+		for(int i = 0; i < size; i++) {
+			if(isPrimitive) {
+				processPrimaryElement(source, target, targetType, i);
+			} else {
+				processObjectElement(source, target, targetType, i);
 			}
-
-			boolean isPrimitive = type.isPrimitive();
-
-			int size = sourceSize < targetSize ? sourceSize : targetSize;
-			for(int i = 0; i < size; i++) {
-				if(isPrimitive) {
-					Array.set(target, i, this.getPrimaryValue(source, i, type));
-				} else if(type.equals(String.class)) {
-					try {
-						Array.set(target, i, source.getString(i));
-					} catch (JSONException e) {
-						Log.e(TAG, "JSONException", e);
-					}
-				} else if(type.equals(JSONObject.class)) {
-					try {
-						Array.set(target, i, source.getJSONObject(i));
-					} catch (JSONException e) {
-						Log.e(TAG, "JSONException", e);
-					}
-				} else if(type.equals(JSONArray.class)) {
-					try {
-						Array.set(target, i, source.getJSONArray(i));
-					} catch (JSONException e) {
-						Log.e(TAG, "JSONException", e);
-					}
-				} else {
-					Object obj = Array.get(target, i);
-					if(obj == null) {
-						try {
-							obj = type.newInstance();
-						} catch (InstantiationException e) {
-							Log.e(TAG, "InstantiationException", e);
-							obj = null;
-						} catch (IllegalAccessException e) {
-							Log.e(TAG, "IllegalAccessException", e);
-							obj = null;
-						}
-					}
-
-					if(obj != null) {
-						try {
-							JSONObject jsonObject = source.getJSONObject(i);
-							obj = this.jsonObjectMapper.map(jsonObject, obj);
-							Array.set(target, i, obj);
-						} catch (JSONException e) {
-							Log.e(TAG, "JSONException", e);
-						}
-					}
-				}
-			}
+		}
 
 		return target;
 	}
 
-	public <T> Object getPrimaryValue(JSONArray source, int index, Class<T> type) {
+	private void processObjectElement(JSONArray source, Target target, Class targetType, int index) {
+		Object obj = Array.get(target, index);
+		if(obj == null) {
+			try {
+				obj = targetType.newInstance();
+			} catch (InstantiationException e) {
+				Log.e(TAG, "InstantiationException", e);
+				obj = null;
+			} catch (IllegalAccessException e) {
+				Log.e(TAG, "IllegalAccessException", e);
+				obj = null;
+			}
+		}
+
+		if(obj != null) {
+			try {
+				JSONObject jsonObject = source.getJSONObject(index);
+				obj = this.jsonObjectMapper.map(jsonObject, obj);
+				Array.set(target, index, obj);
+			} catch (JSONException e) {
+				Log.e(TAG, "JSONException", e);
+			}
+		}
+	}
+
+	private void processPrimaryElement(JSONArray source, Target target, Class targetType, int index) {
 		try {
-			if(!type.isPrimitive()) {
-				return null;
-			}
+			Array.set(target, index, JSONUtil.getPrimaryValue(source, index, targetType));
+		} catch (JSONException ex) {
+			Log.e(TAG, "JSONException", ex);
+		}
+	}
 
-			if(type.equals(Boolean.class) || type.equals(Boolean.TYPE)) {
-				return source.getBoolean(index);
-			}
-			if(type.equals(Double.class) || type.equals(Double.TYPE)) {
-				return source.getDouble(index);
-			} else if(type.equals(Float.class) || type.equals(Float.TYPE)) {
-				return Float.valueOf(String.valueOf(source.getDouble(index)));
-			} else if(type.equals(Integer.class) || type.equals(Integer.TYPE)) {
-				return source.getInt(index);
-			}
-			if(type.equals(Long.class) || type.equals(Long.TYPE)) {
-				return source.getLong(index);
-			}
-
-			return type.cast(source.get(index));
-		} catch (Exception ex) {
-			Log.e(TAG, "Exception", ex);
-			return null;
+	private void prepairJSONObjectMapper() {
+		if(this.jsonObjectMapper == null) {
+			this.jsonObjectMapper = new JSONObjectMapper();
+			this.jsonObjectMapper.setJsonArrayMapper(this);
 		}
 	}
 }
--- core/src/main/java/com/nttuyen/android/umon/core/mapper/JSONObjectMapper.java
+++ core/src/main/java/com/nttuyen/android/umon/core/mapper/JSONObjectMapper.java
@@ -1,7 +1,9 @@
 package com.nttuyen.android.umon.core.mapper;
 
 import android.util.Log;
+import com.nttuyen.android.umon.core.json.JSONUtil;
 import com.nttuyen.android.umon.core.json.Json;
+import com.nttuyen.android.umon.core.reflect.ReflectUtil;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -30,241 +32,162 @@ public class JSONObjectMapper<Target> implements Mapper<JSONObject, Target> {
 
 	@Override
 	public Target map(JSONObject source, Target target) {
-		if(source == null || target == null) {
+		if(source == null || target == null || target.getClass().isPrimitive()) {
 			return target;
+		} else if(target instanceof JSONObject) {
+			return (Target)source;
 		}
 
-		if(this.jsonArrayMapper == null) {
-			this.jsonArrayMapper = new JSONArrayMapper();
-			this.jsonArrayMapper.setJsonObjectMapper(this);
-		}
-
+		repairJSONArrayMapper();
 		Class targetClass = target.getClass();
-		if(targetClass.isPrimitive()) {
-			return target;
-		} else if(targetClass.equals(JSONObject.class)) {
-			target = (Target)source;
-			return target;
-		}
-
-		//TODO: need to process parent field if has
-		Set<Field> fields = new HashSet<Field>();
-
-		//Field declared in current class
-		Field[] fs = targetClass.getDeclaredFields();
-		for(Field f : fs) {
-			fields.add(f);
-		}
-
-		Class parent = targetClass.getSuperclass();
-		while (parent != null && !parent.equals(Object.class)) {
-			fs = parent.getDeclaredFields();
-			for(Field f : fs) {
-				fields.add(f);
-			}
-
-			parent = parent.getSuperclass();
-		}
-
+		Set<Field> fields = ReflectUtil.getAllField(targetClass);
 		for(Field field : fields) {
-			Method setter = this.setterMethod(field, targetClass);
-			if(setter == null) {
-				//This field is readonly
-				continue;
-			}
-			if(!setter.isAccessible()) {
-				setter.setAccessible(true);
-			}
-
-			String jsonField = field.getName();
-			Json json = field.getAnnotation(Json.class);
-			if(json != null && !"".equals(json.name())) {
-				jsonField = json.name();
-			}
-
-			if(source.isNull(jsonField)) {
-				//If has no json value for this field
-				continue;
-			}
-
-			Class fieldType = field.getType();
-			Object fieldValue = null;
-
-			try {
-				if(fieldType.isPrimitive()) {
-					fieldValue = this.getPrimary(source, jsonField, fieldType);
-
-				} else if(fieldType.equals(String.class) || fieldType.equals(JSONObject.class) || fieldType.equals(JSONArray.class)) {
-					fieldValue = fieldType.cast(source.get(jsonField));
-				} else {
-					Method getter = this.getterMethod(field, targetClass);
-					if(!getter.isAccessible()) {
-						getter.setAccessible(true);
-					}
-					if(!field.isAccessible()) {
-						field.setAccessible(true);
-					}
-
-					//Get current fieldValue or create new if is null
-					fieldValue = getter != null ? getter.invoke(target) : field.get(target);
-
-					if(fieldType.isArray()) {
-						if(fieldValue == null) {
-							fieldValue = Array.newInstance(fieldType.getComponentType(), 0);
-						}
-
-						JSONArray jsonArray = source.getJSONArray(jsonField);
-						fieldValue = this.jsonArrayMapper.map(jsonArray, fieldValue);
-					} else if(Collection.class.isAssignableFrom(fieldType)) {
-						if(json == null) {
-							//Cannot to process
-							continue;
-						}
-
-						Class type = json.type();
-						Object array = Array.newInstance(type, 0);
-						JSONArray jsonArray = source.getJSONArray(jsonField);
-						array = this.jsonArrayMapper.map(jsonArray, array);
-
-						if(fieldValue != null) {
-							((Collection)fieldValue).clear();
-							int length = Array.getLength(array);
-							for(int i = 0; i < length; i++) {
-								Object obj = Array.get(array, i);
-								if(obj != null) {
-									((Collection)fieldValue).add(obj);
-								}
-							}
-						} else {
-							if(Set.class.isAssignableFrom(fieldType)) {
-								//Default set instance is hashset
-								fieldValue = new HashSet();
-							} else {
-								//Defaut for collection or list is arraylist
-								fieldValue = new ArrayList();
-							}
-
-							int length = Array.getLength(array);
-							for(int i = 0; i < length; i++) {
-								Object obj = Array.get(array, i);
-								if(obj != null) {
-									((Collection)fieldValue).add(obj);
-								}
-							}
-						}
-					} else {
-						if(fieldValue == null) {
-							try {
-								fieldValue = fieldType.newInstance();
-							} catch (InstantiationException ex) {
-								Log.e(TAG, "InstantiationException", ex);
-								fieldValue = null;
-							}
-						}
-						if(fieldValue == null) {
-							//Only inject for a concrete type
-							continue;
-						}
-
-						JSONObject jsonObject = source.getJSONObject(jsonField);
-						fieldValue = ((JSONObjectMapper)this).map(jsonObject, fieldValue);
-					}
-				}
-			} catch (ClassCastException ex) {
-				Log.e(TAG, "ClassCastException", ex);
-				fieldValue = null;
-			} catch (JSONException ex) {
-				Log.e(TAG, "JSONException", ex);
-				fieldValue = null;
-			} catch (InvocationTargetException e) {
-				Log.e(TAG, "InvocationTargetException", e);
-				fieldValue = null;
-			} catch (IllegalAccessException e) {
-				Log.e(TAG, "IllegalAccessException", e);
-				fieldValue = null;
-			}
-
-			if(fieldValue != null) {
-				try {
-					setter.invoke(target, fieldValue);
-				} catch (IllegalAccessException e) {
-					Log.e(TAG, "IllegalAccessException", e);
-				} catch (InvocationTargetException e) {
-					Log.e(TAG, "InvocationTargetException", e);
-				}
-			}
+			this.processField(source, field, target, targetClass);
 		}
 
 		return target;
 	}
 
-	private Method setterMethod(Field field, Class type) {
-		if(type == null) {
-			type = field.getDeclaringClass();
+	private void repairJSONArrayMapper() {
+		if(this.jsonArrayMapper == null) {
+			this.jsonArrayMapper = new JSONArrayMapper();
+			this.jsonArrayMapper.setJsonObjectMapper(this);
 		}
-		String fieldName = field.getName();
-		String upper = fieldName.toUpperCase();
-		StringBuilder builder = new StringBuilder();
-		builder.append("set")
-				.append(upper.charAt(0))
-				.append(fieldName)
-				.deleteCharAt(4);
+	}
+	private void processField(JSONObject source, Field field, Target target, Class targetClass) {
+		String jsonField = this.getJsonField(field);
+		if(ReflectUtil.setterMethod(field, targetClass) == null || !source.has(jsonField)) {
+			//This field is readonly (has not setter method) or has not contain the key
+			return;
+		}
 
-		String methodName = builder.toString();
+		if(source.isNull(jsonField)) {
+			ReflectUtil.setValueOfField(field, null, target);
+		}
+
 		Class fieldType = field.getType();
+		Object fieldValue = null;
 
-		try {
-			Method method = type.getMethod(methodName, fieldType);
-			return method;
-		} catch (Exception ex) {
-			return null;
+		if(JSONUtil.isPrimaryType(fieldType)) {
+			fieldValue = processFieldWithPrimaryType(source, jsonField, fieldType, fieldValue);
+
+		} else if(fieldType.isArray()) {
+			fieldValue = processFieldWithArrayType(source, jsonField, fieldType, fieldValue);
+
+		} else if(Collection.class.isAssignableFrom(fieldType)) {
+			if(this.getJsonCollectionType(field) != null) {
+				fieldValue = processFieldWithCollectionType(source, field, target, jsonField, fieldType);
+			}
+
+		} else {
+			fieldValue = processFieldWithObjectType(source, field, target, jsonField, fieldType);
+		}
+
+
+		if(fieldValue != null) {
+			ReflectUtil.setValueOfField(field, fieldValue, target);
 		}
 	}
 
-	private Method getterMethod(Field field, Class type) {
-		if(type == null) {
-			type = field.getDeclaringClass();
+	private Object processFieldWithCollectionType(JSONObject source, Field field, Target target, String jsonField, Class fieldType) {
+		Object fieldValue;
+		Class type = this.getJsonCollectionType(field);
+
+
+		Object array = getArrayValues(source, jsonField, type);
+
+		fieldValue = ReflectUtil.getValueOfField(field, target);
+		if(fieldValue == null) {
+			if(Set.class.isAssignableFrom(fieldType)) {
+				fieldValue = new HashSet();
+			} else {
+				fieldValue = new ArrayList();
+			}
 		}
-		String fieldName = field.getName();
-		String upper = fieldName.toUpperCase();
-		StringBuilder builder = new StringBuilder();
-		builder.append("get")
-				.append(upper.charAt(0))
-				.append(fieldName)
-				.deleteCharAt(4);
 
-		String methodName = builder.toString();
+		if(fieldValue != null) {
+			((Collection)fieldValue).clear();
+			int length = Array.getLength(array);
+			for(int i = 0; i < length; i++) {
+				Object obj = Array.get(array, i);
+				if(obj != null) {
+					((Collection)fieldValue).add(obj);
+				}
+			}
+		}
+		return fieldValue;
+	}
 
+	private Object processFieldWithObjectType(JSONObject source, Field field, Target target, String jsonField, Class fieldType) {
+		Object fieldValue;
+		fieldValue = ReflectUtil.getValueOfField(field, target);
+		if(fieldValue == null) {
+			try {
+				fieldValue = fieldType.newInstance();
+			} catch (InstantiationException ex) {
+				Log.e(TAG, "InstantiationException", ex);
+				fieldValue = null;
+			} catch (IllegalAccessException e) {
+				Log.e(TAG, "IllegalAccessException", e);
+			}
+		}
+		if(fieldValue != null) {
+			try {
+				JSONObject jsonObject = source.getJSONObject(jsonField);
+				fieldValue = ((JSONObjectMapper)this).map(jsonObject, fieldValue);
+			} catch (JSONException ex) {
+				Log.e(TAG, "JSONException", ex);
+			}
+		}
+		return fieldValue;
+	}
+
+	private Object getArrayValues(JSONObject source, String jsonField, Class type) {
+		Object array = Array.newInstance(type, 0);
 		try {
-			return type.getMethod(methodName);
-		} catch (NoSuchMethodException e) {
-			Log.e(TAG, "NoSuchMethodException", e);
+			JSONArray jsonArray = source.getJSONArray(jsonField);
+			array = this.jsonArrayMapper.map(jsonArray, array);
+		} catch (JSONException ex) {
+			Log.e(TAG, "JSONException", ex);
 		}
 
+		return array;
+	}
+
+	private Object processFieldWithArrayType(JSONObject source, String jsonField, Class fieldType, Object fieldValue) {
+		try {
+			fieldValue = Array.newInstance(fieldType.getComponentType(), 0);
+			JSONArray jsonArray = source.getJSONArray(jsonField);
+			fieldValue = this.jsonArrayMapper.map(jsonArray, fieldValue);
+		} catch (JSONException ex) {
+			Log.e(TAG, "JSONException", ex);
+		}
+		return fieldValue;
+	}
+
+	private Object processFieldWithPrimaryType(JSONObject source, String jsonField, Class fieldType, Object fieldValue) {
+		try {
+			fieldValue = JSONUtil.getPrimaryValue(source, jsonField, fieldType);
+		} catch (JSONException e) {
+			Log.e(TAG, "JSONException", e);
+		}
+		return fieldValue;
+	}
+
+	private String getJsonField(Field field) {
+		String jsonField = field.getName();
+		Json json = field.getAnnotation(Json.class);
+		if(json != null && !"".equals(json.name())) {
+			jsonField = json.name();
+		}
+
+		return jsonField;
+	}
+	private Class getJsonCollectionType(Field field) {
+		Json json = field.getAnnotation(Json.class);
+		if(json != null && !json.type().equals(Object.class)) {
+			return json.type();
+		}
 		return null;
-	}
-
-	private <T> Object getPrimary(JSONObject json, String fieldName, Class<T> type) throws JSONException {
-		if(!type.isPrimitive()) {
-			return null;
-		}
-
-		if(type.equals(Boolean.class) || type.equals(Boolean.TYPE)) {
-			return json.getBoolean(fieldName);
-		}
-		if(type.equals(Double.class) || type.equals(Double.TYPE)) {
-			return json.getDouble(fieldName);
-		}
-		if(type.equals(Float.class) || type.equals(Float.TYPE)) {
-			return Float.valueOf(String.valueOf(json.getDouble(fieldName)));
-		}
-		if(type.equals(Integer.class) || type.equals(Integer.TYPE)) {
-			return json.getInt(fieldName);
-		}
-		if(type.equals(Long.class) || type.equals(Long.TYPE)) {
-			return json.getLong(fieldName);
-		}
-
-		return type.cast(json.getString(fieldName));
 	}
 }
