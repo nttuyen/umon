cfc4979fdcbe97609653c887b26d4fa4b1907cfc
nttuyen <nttuyen_it@yahoo.com>
2014-09-27 23:39:47 +0700

    update SlidingMenu implement for simpler to use

--- libraries/src/main/java/com/nttuyen/android/umon/widget/SlidingMenu.java
+++ libraries/src/main/java/com/nttuyen/android/umon/widget/SlidingMenu.java
@@ -1,11 +1,13 @@
 package com.nttuyen.android.umon.widget;
 
-import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.os.Build;
+import android.os.Handler;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.*;
 import android.widget.FrameLayout;
@@ -14,10 +16,14 @@ import android.widget.FrameLayout;
  * Created by tuyennt on 9/26/14.
  */
 public class SlidingMenu extends DrawerLayout {
+    private static final String TAG = "UMON:SlidingMenu";
+
     private int width;
     private Activity activity = null;
     private DrawerListener listener = null;
     private int drawerImageResource = android.R.drawable.screen_background_dark_transparent;
+    private int openDrawerContentDescRes = 0;
+    private int closeDrawerContentDescRes = 0;
 
     private final FrameLayout body;
     private final FrameLayout menu;
@@ -28,7 +34,7 @@ public class SlidingMenu extends DrawerLayout {
         this.menu = new FrameLayout(context);
     }
 
-    public void init() {
+    private void init() {
         if(width == 0) {
             if (activity != null){
                 Display display = activity.getWindowManager().getDefaultDisplay();
@@ -51,9 +57,9 @@ public class SlidingMenu extends DrawerLayout {
         menu.setBackgroundColor(getResources().getColor(android.R.color.white));
     }
 
-    public void attachTo(final Activity activity) {
+    public SlidingMenu attachTo(final Activity activity) {
         if(activity == null) {
-            return;
+            return this;
         }
         this.activity = activity;
         this.init();
@@ -74,14 +80,82 @@ public class SlidingMenu extends DrawerLayout {
         }
 
         this.setDrawerImageResource(this.drawerImageResource);
+
+        return this;
     }
 
-    public void setDrawerImageResource(int drawerImageResource) {
+    public SlidingMenu setDrawerImageResource(int drawerImageResource) {
         this.drawerImageResource = drawerImageResource;
         if(this.activity != null) {
-            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(activity, this, drawerImageResource, 0, 0);
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(activity, this, drawerImageResource, this.openDrawerContentDescRes, this.closeDrawerContentDescRes){
+                @Override
+                public void onDrawerOpened(View drawerView) {
+                    super.onDrawerOpened(drawerView);
+                    if (openDrawerContentDescRes != 0) {
+                        try {
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                                activity.getActionBar().setTitle(openDrawerContentDescRes);
+                            } else {
+                                if (activity instanceof ActionBarActivity) {
+                                    ((ActionBarActivity) activity).getSupportActionBar().setTitle(openDrawerContentDescRes);
+                                }
+                            }
+                        } catch (Throwable ex){
+                            Log.w(TAG, "Throwable", ex);
+                        }
+                    }
+                }
+
+                @Override
+                public void onDrawerClosed(View drawerView) {
+                    super.onDrawerClosed(drawerView);
+                    if (closeDrawerContentDescRes != 0) {
+                        try {
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                                activity.getActionBar().setTitle(closeDrawerContentDescRes);
+                            } else if (activity instanceof ActionBarActivity) {
+                                ((ActionBarActivity) activity).getSupportActionBar().setTitle(closeDrawerContentDescRes);
+                            }
+                        } catch (Throwable ex){
+                            Log.w(TAG, "Throwable", ex);
+                        }
+                    }
+
+                }
+            };
             this.setDrawerListener(toggle);
+
+            try {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                    activity.getActionBar().setDisplayHomeAsUpEnabled(true);
+                    activity.getActionBar().setHomeButtonEnabled(true);
+                } else {
+                    if (activity instanceof ActionBarActivity) {
+                        ((ActionBarActivity)activity).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+                        ((ActionBarActivity)activity).getSupportActionBar().setHomeButtonEnabled(true);
+                    }
+                }
+            } catch (Throwable ex) {
+                Log.w(TAG, "Throwable", ex);
+            }
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    SlidingMenu.this.syncState();
+                }
+            }, 1);
         }
+        return this;
+    }
+
+    public SlidingMenu setOpenDrawerContentDescRes(int resourceId) {
+        this.openDrawerContentDescRes = resourceId;
+        return this;
+    }
+
+    public SlidingMenu setCloseDrawerContentDescRes(int resourceId) {
+        this.closeDrawerContentDescRes = resourceId;
+        return this;
     }
 
     @Override
@@ -95,39 +169,49 @@ public class SlidingMenu extends DrawerLayout {
             if (this.listener instanceof ActionBarDrawerToggle) {
                 return ((ActionBarDrawerToggle) listener).onOptionsItemSelected(item);
             }
-        } catch (Exception ex){}
+        } catch (Throwable ex){
+            Log.w(TAG, "Throwable", ex);
+        }
 
         return false;
     }
 
-    public void syncState() {
+    public SlidingMenu syncState() {
         try {
             if (this.listener instanceof ActionBarDrawerToggle) {
                 ((ActionBarDrawerToggle) listener).syncState();
             }
-        } catch (Exception ex){}
+        } catch (Throwable ex){
+            Log.w(TAG, "Throwable", ex);
+        }
+
+        return this;
     }
 
-    public void toggle() {
+    public SlidingMenu toggle() {
         if(this.isDrawerOpen(menu)) {
             this.closeDrawer(menu);
         } else {
             this.openDrawer(menu);
         }
+        return this;
     }
 
-    public void setWidth(int width) {
+    public SlidingMenu setWidth(int width) {
         this.width = width;
         ViewGroup.LayoutParams params = this.menu.getLayoutParams();
         if(params != null) {
             params.width = width;
         }
+        return this;
     }
-    public void setContentView(View view) {
+    public SlidingMenu setContentView(View view) {
         FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         this.menu.addView(view, params);
+
+        return this;
     }
-    public void setContentView(int layoutId) {
-        setContentView(LayoutInflater.from(getContext()).inflate(layoutId, null));
+    public SlidingMenu setContentView(int layoutId) {
+        return setContentView(LayoutInflater.from(getContext()).inflate(layoutId, null));
     }
 }
